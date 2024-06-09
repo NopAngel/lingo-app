31c1cdf7c0c08466ff47b0130d16d5314df31df4
NoAngel <155915830+Angel45003@users.noreply.github.com>
2024-06-08 20:26:00 -0400

    page.jsx

--- app/challenges/boss/page.jsx
+++ app/challenges/boss/page.jsx
@@ -6,7 +6,7 @@ function Boss() {
     <main className="flex justify-center mt-[20px]">
       <section>
         <h1 className="text-3xl font-extrabold p-2 max-w-2xl text-center">
-          "A dog was walking and met a horse."
+          A dog was walking and met a horse.
         </h1>
         <h2 className="w-fit mx-auto">¿Qué se encontro el Perro?</h2>
 
